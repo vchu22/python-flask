5de32a441c296ef406fb6d729e44d661a4f20ed8
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-08-04 22:46:55 -0400

    Working with the GET method

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
 @app.route("/index")
@@ -17,7 +17,14 @@ def courses(term="Spring 2019"):
     coursesData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
     return render_template("courses.html", coursesData=coursesData, courses=True, term=term)
 
-
 @app.route("/register")
 def register():
     return render_template("register.html", register=True)
+
+@app.route("/enrollment")
+def enrollment():
+    id = request.args.get("courseID")
+    title = request.args.get("title")
+    term = request.args.get("term")
+
+    return render_template("enrollment.html", enrollment=True, data={"id": id, "title": title, "term": term})
